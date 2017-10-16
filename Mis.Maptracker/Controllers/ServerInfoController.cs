c563aba279e7fd9b6d77c13f59f9ddf42bf4f0db
Phillip Hancock <phillip.hancock@shred-it01.shred-it.com>
2017-10-16 11:55:56 -0400

    Add project files.

---
+++ Mis.Maptracker/Controllers/ServerInfoController.cs
@@ -0,0 +1,40 @@
+﻿using Mis.Maptracker.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Mis.Maptracker.Controllers
+{
+    public class ServerInfoController : ApiController
+    {
+        public ServerInfo Get(string IP = "162.244.55.44", int Port = 64152)
+        {
+            var serverINfo = new A2S_INFO(new IPEndPoint(IPAddress.Parse(IP), Port));
+            return new ServerInfo(serverINfo);
+        }
+    }
+
+    public class ServerInfo
+    {
+        public string Name { get; set; }
+        public int MaxPlayers { get; set; }
+        public int NumPlayers { get; set; }
+
+        public string TimeOfDay { get; set; }
+        public ServerInfo(A2S_INFO from)
+        {
+            var tags = from.Keywords.Split(';').ToArray();
+            Name = from.Name;
+            MaxPlayers = from.MaxPlayers;
+            NumPlayers = from.Players;
+            TimeOfDay = tags[0];
+            int numPlayers = 0;
+            if (Int32.TryParse(tags[1], out numPlayers))
+                if (NumPlayers != numPlayers)
+                    NumPlayers = numPlayers;
+        }
+    }
+}
